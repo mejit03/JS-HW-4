86c59f60c9e231457dae18a742e91d69a48e8f4d
mejit03 <dtghjgfhfdh@gmail.com>
2022-11-19 13:59:13 +0300

    JS-HW-4

---
+++ app.js
@@ -0,0 +1,47 @@
+// const increment = document.querySelector('.increment');
+// const decrement = document.querySelector('.decrement');
+// const num = document.querySelector('#num');
+
+// let clicks = 0;
+
+
+// increment.addEventListener("click", () => {
+//     clicks += 1;
+//     num.innerHTML = clicks;
+//     num.style.color = "green";
+// });
+
+// decrement.addEventListener("click", () => {
+//     clicks -= 1;
+//     if(clicks >0){
+//         num.innerHTML = clicks;
+//     } else if (clicks <= 0) {
+//         num.innerHTML = 0;
+//         clicks = 0;
+//     }
+//     num.style.color = "red";
+// });
+
+
+
+// document.querySelector('.block').onmousemove = function(event) {
+//     event = event;
+//     document.querySelector('.x').innerHTML = event.offsetX;
+//     document.querySelector('.y').innerHTML = event.offsetY;
+// }
+
+
+
+
+// const redTraffic = document.querySelector('.redTraffic');
+// const greenTraffic = document.querySelector('.greenTraffic');
+
+// const state = prompt('красный или зеленый');
+
+// if(state == "красный"){
+//     redTraffic.style.background = "red";
+//     alert("STOP");
+// }else if(state == "зеленый"){
+//     greenTraffic.style.background = "rgb(4, 230, 4)";
+//     alert("GO");
+// }
